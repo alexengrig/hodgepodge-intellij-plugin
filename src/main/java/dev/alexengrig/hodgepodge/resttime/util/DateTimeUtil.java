a67f95a20734837297b21b6561577f906e6b7b9d
Grig Alex <alexengrigdev@gmail.com>
2020-03-08 22:58:35 +0300

    Add DateTimeUtil#toLocalDateTime

--- src/main/java/dev/alexengrig/hodgepodge/resttime/util/DateTimeUtil.java
+++ src/main/java/dev/alexengrig/hodgepodge/resttime/util/DateTimeUtil.java
@@ -1,5 +1,6 @@
 package dev.alexengrig.hodgepodge.resttime.util;
 
+import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 
@@ -13,5 +14,13 @@ public final class DateTimeUtil {
 
     public static long toLong(LocalDateTime dateTime, ZoneId zoneId) {
         return dateTime.atZone(zoneId).toInstant().toEpochMilli();
+    }
+
+    public static LocalDateTime toLocalDateTime(long epochMilli) {
+        return toLocalDateTime(epochMilli, ZoneId.systemDefault());
+    }
+
+    public static LocalDateTime toLocalDateTime(long epochMilli, ZoneId zoneId) {
+        return Instant.ofEpochMilli(epochMilli).atZone(zoneId).toLocalDateTime();
     }
 }
