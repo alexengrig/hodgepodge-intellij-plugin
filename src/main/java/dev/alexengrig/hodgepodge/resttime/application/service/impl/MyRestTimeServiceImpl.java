bafc3d556360841551a8aa6f565cfc535659eb3f
Grig Alex <alexengrigdev@gmail.com>
2020-03-08 19:09:54 +0300

    Refactor state to MyRestTimeServiceImpl

--- src/main/java/dev/alexengrig/hodgepodge/resttime/application/service/impl/MyRestTimeServiceImpl.java
+++ src/main/java/dev/alexengrig/hodgepodge/resttime/application/service/impl/MyRestTimeServiceImpl.java
@@ -2,38 +2,32 @@ package dev.alexengrig.hodgepodge.resttime.application.service.impl;
 
 import com.intellij.openapi.components.PersistentStateComponent;
 import com.intellij.openapi.components.State;
+import com.intellij.openapi.components.Storage;
 import dev.alexengrig.hodgepodge.resttime.application.service.MyRestTimeService;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.time.LocalDateTime;
+import java.io.Serializable;
+import java.util.Date;
 
-@State(name = "MyRestTimeServiceImpl")
+@State(name = "MyRestTimeServiceImpl", storages = @Storage("restTime.xml"))
 public class MyRestTimeServiceImpl implements MyRestTimeService, PersistentStateComponent<MyRestTimeServiceImpl.State> {
-    private State myState;
-    private int workTime;
-    private int restTime;
-
-    public MyRestTimeServiceImpl() {
-        myState = new State();
-        workTime = 45;
-        restTime = 15;
-    }
+    private State myState = new State();
 
     @Override
     public void updateTimes(int workTime, int restTime) {
-        this.workTime = workTime;
-        this.restTime = restTime;
+        myState.workTime = workTime;
+        myState.restTime = restTime;
     }
 
     @Override
     public int getWorkTime() {
-        return workTime;
+        return myState.workTime;
     }
 
     @Override
     public int getRestTime() {
-        return restTime;
+        return myState.restTime;
     }
 
     @Nullable
@@ -45,9 +39,14 @@ public class MyRestTimeServiceImpl implements MyRestTimeService, PersistentState
     @Override
     public void loadState(@NotNull State state) {
         myState = state;
+        System.out.println("HERE");
     }
 
-    protected static class State {
-        private LocalDateTime dateTime = LocalDateTime.now();
+    static class State implements Serializable {
+        public int workTime = 45; //TODO: set 45
+        public int restTime = 5; //TODO: set 5
+        public boolean isRest = false;
+        public long start = new Date().getTime();
+        public long end = 0;
     }
 }
